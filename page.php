5d590ad635a4912a38500774c31f0bf930c211d9
Shane Moloney <shanemoloney3@gmail.com>
2026-03-12 15:08:27 +0100

    Update sponsor grid style and remove featured image from top of pages

--- page.php
+++ page.php
@@ -5,11 +5,7 @@
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         
         <article>
-            <?php if (has_post_thumbnail()) : ?>
-            <div class="rounded-2xl overflow-hidden mb-12 aspect-video">
-                <?php the_post_thumbnail('large', array('class' => 'w-full h-full object-cover')); ?>
-            </div>
-            <?php endif; ?>
+
 
             <h1 class="text-4xl md:text-5xl font-black mb-12 leading-tight">
                 <?php the_title(); ?>
